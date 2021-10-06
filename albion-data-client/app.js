3d93be5b23c8b524cfbc239ad928570fb52f7b25
Robin M <robinmacmillan0@gmail.com>
2021-10-06 15:17:20 -0700

    Updated main with modules

--- albion-data-client/app.js
+++ albion-data-client/app.js
@@ -1,20 +1,33 @@
-const { connect } = require("nats");
-(async () => {
+const { JSONCodec } = require('nats')
+const cron = require('node-cron')
+const { natsConnector } = require('./natsConnector')
+const { mongoConnector } = require('./mongoConnector')
+const { upsertMarketOrder } = require('./upsertMarketOrder')
+const { deleteMarketOrders } = require('./deleteMarketOrders')
+const { MarketOrder } = require('./model.js')
 
-  const nc = await connect({ 
-    servers: "www.albion-online-data.com:4222",
-    user: "public",
-    pass: "thenewalbiondata",
-    json: true,
+//Big thanks to the Albion Data Project
+//https://www.albion-online-data.com/
+
+async function main() {
+  //Establish subscription to NATS stream and connect to MongoDB
+  //Connection deets can be changed in their respective modules
+  const sub = await natsConnector();
+  await mongoConnector();
+
+  //Cron job to check for and delete expired orders
+  //Currently checks every minute and deletes orders that haven't been updated in over 24 hours
+  cron.schedule('* * * * *', () => {
+    deleteMarketOrders();
   });
 
-  console.log(`Connected to ${nc.getServer()}`);
+  //Codec to translate byte array into JSON
+  const codec = JSONCodec();
 
-  const sub = nc.subscribe("marketorders.deduped");
+  //Each message recieved will either update or create a document
+  for await (const msg of sub) {
+    upsertMarketOrder(MarketOrder, codec.decode(msg.data));
+  }
+}
 
-    for await (const m of sub) {
-      console.log(`[${sub.getProcessed()}]: ${m.data}`);
-    }
-
-  await nc.drain();
-})().catch(console.log);
+main().catch(console.error);
