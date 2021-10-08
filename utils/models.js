45e5eb688bc1995419dd4c7669c6281698929da1
Robin M <robinmacmillan0@gmail.com>
2021-10-07 21:11:59 -0700

    Created profitorder model

--- utils/models.js
+++ utils/models.js
@@ -2,19 +2,43 @@ const mongoose = require('mongoose')
 const { Schema } = mongoose;
 
 const marketOrderSchema = new Schema({
-    _id: Number,
-    ItemTypeId: String,
-    LocationId: Number,
-    QualityLevel: Number,
-    EnchantmentLevel: Number,
-    UnitPriceSilver: Number,
-    Amount: Number,
-    AuctionType: String,
-    Expires: Date,
-    ItemGroupTypeId: String
+  _id: Number,
+  ItemTypeId: String,
+  LocationId: Number,
+  QualityLevel: Number,
+  EnchantmentLevel: Number,
+  UnitPriceSilver: Number,
+  Amount: Number,
+  AuctionType: String,
+  Expires: Date,
+  ItemGroupTypeId: String
+},
+{ timestamps: true });
+ 
+const MarketOrder = mongoose.model('MarketOrder', marketOrderSchema);
+
+const profitOrderSchema = new Schema({
+  Item: {
+    Id: String,
+    Name: String,
+    Enchantement: Number,
+    Quality: Number
   },
-  { timestamps: true }
-  );
-  
-  const MarketOrder = mongoose.model('MarketOrder', marketOrderSchema);
-  module.exports = { MarketOrder };
+  BuyFrom: {
+    Id: Number,
+    Name: String
+  },
+  SellTo: {
+    Id: Number,
+    Name: String
+  },
+  Quantity: Number,
+  TotalBuyPrice: Number,
+  TotalSellPrice: Number,
+  Profit: Number
+},
+{ timestamps: true });
+
+const ProfitOrder = mongoose.model('ProfitOrder', profitOrderSchema);
+
+module.exports = { MarketOrder, ProfitOrder };
