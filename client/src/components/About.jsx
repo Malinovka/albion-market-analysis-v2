fce1d557fb3582205079e176bbd5ad37ac9180fd
Robin M <robinmacmillan0@gmail.com>
2021-11-13 19:33:08 -0800

    Updated about and styling

--- client/src/components/About.jsx
+++ client/src/components/About.jsx
@@ -1,16 +1,22 @@
 import Button from "./Button"
+import img from '../imgs/gold.png'
 
 export default function About(props) {
     return (
         <div className='modal'>
             <div className='about'>
                 <Button className='close-btn' handleClick={props.handleExit}>X</Button>
-                <h4>Real time market analysis for Albion Online</h4>
-                <p>We calculate the most profitable trades currently on the market, 
-                all you have to do is buy from one location and sell to another for maximum profit.</p>
-                <p>Made possible thanks to the Albion Data Project. 
-                Want the most up to date deals? Download their client! 
-                The more data we have, the more accurate our tool is.</p>
+                <div className='about-title'><h4>Real-time market analysis for Albion Online</h4></div>
+                    <div className='about-content'>
+                    <img src={img}/>
+                        <p>We calculate the most profitable trades on the market, 
+                        all you have to do is buy from one location and sell to another for maximum profit.</p>
+                        <p>Made possible thanks to the <a href='https://www.albion-online-data.com/' target='_blank'>Albion Online Data Project</a>. 
+                        Want the most up to date deals? Download their client! 
+                        The more data we have, the more accurate our tool is.</p>
+                        <p>Want to follow this project? <a href='https://github.com/Malinovka/albion-market-analysis-v2' target='_blank'>Check it out on GitHub</a>.</p>
+                        
+                </div>
             </div>
         </div>
     )
